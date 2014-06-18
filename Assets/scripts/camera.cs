f1e906fd2fb86c70d462a7b30da0fc807132ef8e
KaiGeffen <geffenkai@gmail.com>
2014-06-18 16:56:25 -0400

    Made camera slow rotation and zoom work and its awesome

--- Assets/scripts/camera.cs
+++ Assets/scripts/camera.cs
@@ -3,41 +3,111 @@ using System.Collections;
 
 using System;
 
-public class camera : MonoBehaviour {
+public class camera : MonoBehaviour
+{
 
-	public GameObject focus;
 	/// <summary>
-	/// Distance from camera to focus
+	/// Object that camera follows and focuses on
+	/// </summary>
+	public GameObject subject;
+	/// <summary>
+	/// Current distance from camera to subject
 	/// </summary>
 	public float distance;
 	/// <summary>
-	/// Angle up from ground that camera points at focus. In degrees
+	/// Maximum distance between camera and subject 
+	/// </summary>
+	public float maxDist;
+	/// <summary>
+	/// Minimum distance between camera and subject
+	/// </summary>
+	public float minDist;
+	/// <summary>
+	/// How far towards/away from subject camera moves each second
+	/// </summary>
+	public float distIncrement;
+
+	/// <summary>
+	/// Current angle (up from xz plane) that camera points at subject. In degrees
 	/// </summary>
 	public float angle;
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
 	/// <summary>
-	/// Position camera in correct location and orientation pointed at focus
+	/// Number of seconds until real rotation matches target rotation
 	/// </summary>
-	void Update () {
-		// set camera position to *distance* back from *focus*
-		gameObject.transform.position = focus.transform.position + new Vector3 (0, 0, -distance);
+	public float rotSlowTime;
 
-		// reset rotation, must be done before rotation adjustments get made
-		gameObject.transform.rotation = new Quaternion ();
+
+	void Start () {
+		// Move camera to _distance back from _subject
+		transform.position = subject.transform.position + new Vector3 (0, 0, -distance);
+
+		// ROTATION SETUP
+		// Reset rotation, must be done before rotation adjustments get made
+		transform.rotation = new Quaternion ();
 
 		// rotate up from ground by *angle* degrees
-		gameObject.transform.RotateAround (focus.transform.position,
+		transform.RotateAround (subject.transform.position,
 		                                  new Vector3 (1, 0, 0),
 		                                  angle);
 		
 		// rotate around y axis 45 degrees to get angled view
-		gameObject.transform.RotateAround (focus.transform.position,
+		transform.RotateAround (subject.transform.position,
 		                                   new Vector3 (0, 1, 0),
 		                                   45);
+	}
+
+	void Update ()
+	{
+		handleZoom ();
+		handleRotation ();
+	}
+
+	/// <summary>
+	/// Angle of rotation around subject that camera aims to achieve
+	/// Gets to this angle over several frames
+	/// </summary>
+	public double targetRot = 0;
+	/// <summary>
+	/// Number of seconds until slow rotation finishes
+	/// </summary>
+	public double timeToRot;
+	/// <summary>
+	/// Gradually rotates based on keypress
+	/// </summary>
+	void handleRotation ()
+	{
+		// Determine targetRot
+		if (Input.GetButtonDown ("RotRight"))
+			targetRot += 90;
+		else if (Input.GetButtonDown ("RotLeft"))
+			targetRot -= 90;
+
+		if (targetRot >= 360)
+			targetRot -= 360;
+		else if (targetRot <= -360)
+			targetRot += 360;
+
+		// Rotate towards targetRot
+		// Amount to rotate this tic
+		double dRot = (targetRot / 2) * Time.deltaTime / timeToRot;
+		targetRot -= dRot;
+
+		gameObject.transform.RotateAround (subject.transform.position,
+		                                   new Vector3 (0, 1, 0),
+		                                   (float)dRot);
+	}
+
+	/// <summary>
+	/// Zoom in/out based on zoom axis input
+	/// </summary>
+	void handleZoom ()
+	{
+		float z = Input.GetAxis ("Zoom");
+
+		float potentialDistance = distance - z * distIncrement * Time.deltaTime;
+
+		if (potentialDistance >= minDist && potentialDistance <= maxDist)
+			distance = potentialDistance;
 	}
 }
